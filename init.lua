a60d1c3f1a441bf151fea42414f4f3692eac192f
endoffline <stefan.hoeller93@gmail.com>
2024-11-07 13:54:14 +0100

    Adapt LSPs

--- init.lua
+++ init.lua
@@ -710,7 +710,7 @@ require('lazy').setup({
         --    https://github.com/pmizio/typescript-tools.nvim
         --
         -- But for many setups, the LSP (`ts_ls`) will work just fine
-        -- ts_ls = {},
+        ts_ls = {},
         --
         angularls = {
           root_dir = lsp_util.root_pattern('nx.json', 'project.json', '.git'),
@@ -735,6 +735,13 @@ require('lazy').setup({
               -- diagnostics = { disable = { 'missing-fields' } },
             },
           },
+        },
+
+        java_language_server = {
+          root_dir = lsp_util.root_pattern('build.gradle', '.git'),
+        },
+        gradle_ls = {
+          root_dir = lsp_util.root_pattern('build.gradle', '.git'),
         },
       }
 
