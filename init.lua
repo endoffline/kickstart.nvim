b9aba86e39b164b9c787b0d7a0eff8dcd65f5b32
endoffline <stefan.hoeller93@gmail.com>
2024-11-07 13:52:04 +0100

    Add autocmd for floating diagnostics

--- init.lua
+++ init.lua
@@ -672,6 +672,29 @@ require('lazy').setup({
                 vim.api.nvim_clear_autocmds { group = 'kickstart-lsp-highlight', buffer = event2.buf }
               end,
             })
+
+            -- Autocommand to open a floating window with diagnostics when the cursor is held
+            vim.api.nvim_create_autocmd({ 'CursorHold' }, {
+              pattern = '*',
+              callback = function()
+                for _, winid in pairs(vim.api.nvim_tabpage_list_wins(0)) do
+                  if vim.api.nvim_win_get_config(winid).zindex then
+                    return
+                  end
+                end
+                vim.diagnostic.open_float {
+                  scope = 'cursor',
+                  focusable = false,
+                  close_events = {
+                    'CursorMoved',
+                    'CursorMovedI',
+                    'BufHidden',
+                    'InsertCharPre',
+                    'WinLeave',
+                  },
+                }
+              end,
+            })
           end
 
           -- The following code creates a keymap to toggle inlay hints in your
