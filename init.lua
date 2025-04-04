063ef75c68eb3008e3caf0e51870e0ed3eef1f29
endoffline <stefan.hoeller93@gmail.com>
2025-04-04 14:43:03 +0200

    Adapted which key description for <leader>-c

--- init.lua
+++ init.lua
@@ -344,6 +344,7 @@ require('lazy').setup({
 
       -- Document existing key chains
       spec = {
+        { '<leader>c', group = '[C]opilot', mode = { 'n', 'x' } },
         { '<leader>s', group = '[S]earch' },
         { '<leader>t', group = '[T]oggle' },
         { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
