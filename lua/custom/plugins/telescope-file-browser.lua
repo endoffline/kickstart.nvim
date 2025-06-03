4f04dcf3d6de3fe289b86b1ec80acd45ea840512
endoffline <stefan.hoeller93@gmail.com>
2025-06-03 11:23:00 +0200

    Simplify file browser config and disable netrw

--- lua/custom/plugins/telescope-file-browser.lua
+++ lua/custom/plugins/telescope-file-browser.lua
@@ -3,25 +3,32 @@ return {
     'nvim-telescope/telescope-file-browser.nvim',
     dependencies = { 'nvim-telescope/telescope.nvim', 'nvim-lua/plenary.nvim' },
     config = function()
-      require('telescope').load_extension 'file_browser'
+      local telescope = require 'telescope'
+      telescope.setup {
+        extensions = {
+          file_browser = {
+            theme = 'dropdown',
+            hijack_netrw = true, -- This will make the file browser take over netrw
+            path = vim.fn.expand '%:p:h',
+            select_buffer = true,
+            hidden = true,
+          },
+        },
+      }
+      telescope.load_extension 'file_browser'
 
       -- Keybinding to open the file browser with the path of the current buffer
       require('which-key').add {
         {
           '<leader>b',
           function()
-            require('telescope').extensions.file_browser.file_browser { path = vim.fn.expand '%:p:h', select_buffer = true, hidden = true }
+            telescope.extensions.file_browser.file_browser {}
           end,
           desc = 'File [B]rowser',
           silent = true,
           noremap = true,
         },
       }
-
-      -- Disable netrw
-      -- vim.g.loaded_netrw = 1
-
-      -- vim.g.loaded_netrwPlugin = 1
     end,
   },
 }
